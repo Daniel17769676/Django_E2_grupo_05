80751911a34bdb41bc1a1652e3a908dd1253627c
ni <revecoFuentesd@gmail.com>
2024-04-01 19:05:03 -0300

    agregue las url, y las view

--- proyectoUno/web/urls.py
+++ proyectoUno/web/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path #Daniel: Importamos la funcion path
-from .views import home #Daniel: Importamos la funcion home del archivo views.py
+from .views import (Animales, Armas, Construcciones, Consumibles,
+                    Enemigos, Flora, forowiki, historia,
+                    inicio_sesion, Logros, Lugares,
+                    menuprincipal_wiki, micuentatf, recuperarcontra,
+                    registrase_wiki) #Daniel: Importamos la funcion home del archivo views.py
 
 urlpatterns = [
-    path('', home, name='home'),
+   
+    path('',Animales, name="Animales"),
+    path('',Armas,name="Armas"),
+    path('',Construcciones,name="Construcciones"),
+    path('',Consumibles,name="Consumibles"),
+    path('',Enemigos,name="Enemigos"),
+    path('',Flora,name="Flora"),
+    path('',forowiki,name="forowiki"),
+    path('',historia,name="historia"),
+    path('',inicio_sesion,name="inicio_sesion"),
+    path('',Logros,name="Logros"),
+    path('',Lugares,name="Lugares"),
+    path('',menuprincipal_wiki,name="menuprincipal"),
+    path('',micuentatf,name="micuentatf"),
+    path('',recuperarcontra,name="recuperarcontra"),
+    path('',registrase_wiki,name="registrase"),
+
 ] #Daniel: Aqui se crea una lista de urls que se van a utilizar en el proyecto, este el paso 3
 
--- proyectoUno/web/views.py
+++ proyectoUno/web/views.py
@@ -2,5 +2,49 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'web/menuprincipal_wiki.html')#Daniel: Aqui se llama al archivo html que se va a renderizar (renderizar es mostrar en pantalla), este es el paso 3
+#Daniel: Aqui se llama al archivo html que se va a renderizar (renderizar es mostrar en pantalla), este es el paso 3
+
+def Animales(request):
+    return render(request, 'web/Animales.html')
+
+def Armas(request):
+    return render(request, 'web/Armas.html')
+
+def Construcciones(request):
+    return render(request, 'web/Construcciones.html')
+
+def Consumibles(request):
+    return render(request, 'web/Consumibles.html')
+
+def Enemigos(request):
+    return render(request, 'web/Enemigos.html')
+
+def Flora(request):
+    return render(request, 'web/Flora.html')
+
+def forowiki(request):
+    return render(request, 'web/forowiki.html')
+
+def historia(request):
+    return render(request, 'web/historia.html')
+
+def inicio_sesion(request):
+    return render(request, 'web/inicio_sesion.html')
+
+def Logros(request):
+    return render(request, 'web/Logros.html')
+
+def Lugares(request):
+    return render(request, 'web/Lugares.html')
+
+def menuprincipal_wiki(request):
+    return render(request, 'web/menuprincipal_wiki.html')
+
+def micuentatf(request):
+    return render(request, 'web/micuentatf.html')
+
+def recuperarcontra(request):
+    return render(request, 'web/recuperarconta.html')
+
+def registrase_wiki(request):
+    return render(request, 'web/registrase_wiki.html')
